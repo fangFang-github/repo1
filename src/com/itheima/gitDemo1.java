2b334372fe7edcba3a60ad73d643aa74a60af70e
fangFang <991532948@qq.com>
2019-05-07 16:00:08 +0800

    提交一个idea集成git版本管理工具测试工程,第二次

--- src/com/itheima/gitDemo1.java
+++ src/com/itheima/gitDemo1.java
@@ -2,7 +2,7 @@ package com.itheima;
 
 public class gitDemo1 {
     public static void main(String[] args) {
-        System.out.println("idea集成git版本管理工具...");
+        System.out.println("idea集成git版本管理工具......");
     }
 
 }
