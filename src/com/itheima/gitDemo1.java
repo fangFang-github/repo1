83af2aab0b08c290954e383debc3e0fcb9f28e79
fangFang <991532948@qq.com>
2019-05-07 18:10:30 +0800

    修改之后再次推到git-hub远程仓库

--- src/com/itheima/gitDemo1.java
+++ src/com/itheima/gitDemo1.java
@@ -3,6 +3,8 @@ package com.itheima;
 public class gitDemo1 {
     public static void main(String[] args) {
         System.out.println("idea集成git版本管理工具......");
+
+        System.out.println("修改之后再次推到git-hub远程仓库");
         
     }
 
